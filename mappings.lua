a5772224e800f2e4e0e202f2b6353d2d2034b8a8
LAU <riotrooper@outlook.com>
2023-04-04 21:49:15 +0800

    Version 0.1.0

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -23,6 +25,10 @@ return {
   },
   t = {
     -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
   },
+  v = {
+    ["<A-k>"] = { ":move '<-2<CR>gv-gv", desc = "Move text down" },
+    ["<A-j>"] = { ":move '>+1<CR>gv-gv", desc = "Move text up" },
+  },
+  -- ["<esc>"] = false,
 }
